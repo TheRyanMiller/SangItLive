8cd49812902f18f018d1fcf4d608fe0ac05c1a51
Ryan <rmiller07@gmail.com>
2016-08-01 22:18:39 -0400

    first commit

---
+++ app/src/main/java/com/rtmillerprojects/sangitlive/MainActivity.java
@@ -0,0 +1,124 @@
+package com.rtmillerprojects.sangitlive;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView responseText;
+    private TextView titleText;
+    private Button searchButton;
+    private static final int CONNECTION_TIMEOUT = 150000;
+    private static final int DATARETRIEVAL_TIMEOUT = 150000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        responseText = (TextView) findViewById(R.id.response);
+        searchButton = (Button) findViewById(R.id.searchButton);
+        final Context context = this;
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,"clicked button",Toast.LENGTH_SHORT).show();
+                requestWebService("http://api.setlist.fm/rest/0.1/artist/a74b1b7f-71a5-4011-9441-d0b5e4122711/setlists.json");
+            }
+        });
+    }
+
+    public static JSONObject requestWebService(String serviceUrl) {
+        disableConnectionReuseIfNecessary();
+
+        HttpURLConnection urlConnection = null;
+        try {
+            // create connection
+            URL urlToRequest = new URL(serviceUrl);
+            urlConnection = (HttpURLConnection) urlToRequest.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+            //urlConnection.setConnectTimeout(CONNECTION_TIMEOUT);
+            //urlConnection.setReadTimeout(DATARETRIEVAL_TIMEOUT);
+
+            // handle issues
+            int statusCode = urlConnection.getResponseCode();
+            if (statusCode == HttpURLConnection.HTTP_UNAUTHORIZED) {
+                // handle unauthorized (if service requires user login)
+            } else if (statusCode != HttpURLConnection.HTTP_OK) {
+                // handle any other errors, like 404, 500,..
+            }
+
+            // create JSON object from content
+            InputStream stream = urlConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(stream);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            return new JSONObject(getResponseText(in));
+
+        } catch (MalformedURLException e) {
+            // URL is invalid
+        } catch (SocketTimeoutException e) {
+            // data retrieval or connection timed out
+        } catch (IOException e) {
+            // could not read response body
+            // (could not create input stream)
+        } catch (JSONException e) {
+            // response body is no valid JSON string
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * required in order to prevent issues in earlier Android version.
+     */
+    private static void disableConnectionReuseIfNecessary() {
+        // see HttpURLConnection API doc
+        if (Integer.parseInt(Build.VERSION.SDK)
+                < Build.VERSION_CODES.FROYO) {
+            System.setProperty("http.keepAlive", "false");
+        }
+    }
+
+    private static String getResponseText(InputStream inStream) {
+        // very nice trick from
+        // http://weblogs.java.net/blog/pat/archive/2004/10/stupid_scanner_1.html
+        return new Scanner(inStream).useDelimiter("\\A").next();
+    }
+}
